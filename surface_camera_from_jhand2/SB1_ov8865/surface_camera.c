78c64a5441437a4f20968d10d37a91104785ef07
Tsuchiya Yuto <kitakar@gmail.com>
2020-06-24 21:03:12 +0900

    surface_camera: SB1_ov8865: perform i2c_unregister_device() on driver exit
    Fixes the following error on next driver load:
    
        [   75.811626] kernel: sysfs: cannot create duplicate filename '/kernel/software_nodes/INT33BE'
        [   75.811630] kernel: CPU: 1 PID: 2753 Comm: modprobe Tainted: G         C OE     5.8.0-rc2-1-surface-mainline #1
        [   75.811632] kernel: Hardware name: Microsoft Corporation Surface Book/Surface Book, BIOS 92.3192.768 03.24.2020
        [   75.811632] kernel: Call Trace:
        [   75.811645] kernel:  dump_stack+0x6b/0x88
        [   75.811651] kernel:  sysfs_warn_dup.cold+0x17/0x2d
        [   75.811654] kernel:  sysfs_create_dir_ns+0xc6/0xe0
        [   75.811659] kernel:  kobject_add_internal+0xab/0x2f0
        [   75.811662] kernel:  kobject_init_and_add+0x8b/0xc0
        [   75.811670] kernel:  swnode_register+0x10d/0x1f0
        [   75.811673] kernel:  software_node_register_nodes+0x62/0xe0
        [   75.811677] kernel:  ? 0xffffffffc0b8e000
        [   75.811680] kernel:  surface_camera_init+0x13/0x1000 [surface_camera]
        [   75.811684] kernel:  do_one_initcall+0x59/0x240
        [   75.811690] kernel:  do_init_module+0x5c/0x260
        [   75.811693] kernel:  load_module+0x21a5/0x2450
        [   75.811700] kernel:  __do_sys_init_module+0x12d/0x180
        [   75.811706] kernel:  do_syscall_64+0x44/0x70
        [   75.811710] kernel:  entry_SYSCALL_64_after_hwframe+0x44/0xa9
        [   75.811712] kernel: RIP: 0033:0x7f6d0041773e
        [   75.811713] kernel: Code: Bad RIP value.
        [   75.811714] kernel: RSP: 002b:00007ffc163e5dd8 EFLAGS: 00000246 ORIG_RAX: 00000000000000af
        [   75.811716] kernel: RAX: ffffffffffffffda RBX: 0000558f56991f40 RCX: 00007f6d0041773e
        [   75.811717] kernel: RDX: 0000558f5656a368 RSI: 0000000000002561 RDI: 0000558f56995ca0
        [   75.811718] kernel: RBP: 0000558f56995ca0 R08: 0000000000000000 R09: 00007ffc163e41e1
        [   75.811720] kernel: R10: 0000000000000001 R11: 0000000000000246 R12: 0000558f5656a368
        [   75.811721] kernel: R13: 0000000000000000 R14: 0000558f569920f0 R15: 0000558f56991f40
        [   75.811724] kernel: kobject_add_internal failed for INT33BE with -EEXIST, don't try to register things with the same name in the same directory.
    
    References:
    - https://github.com/torvalds/linux/blob/194098915ac74daddca9d6ed46fd11be57f45e16/drivers/platform/x86/intel_cht_int33fe_typec.c#L354-L356
    
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- surface_camera_from_jhand2/SB1_ov8865/surface_camera.c
+++ surface_camera_from_jhand2/SB1_ov8865/surface_camera.c
@@ -149,7 +149,7 @@ out:
 static void __exit surface_camera_exit(void)
 {
 	camera.cio2->dev.fwnode = ERR_PTR(-ENODEV);
-	camera.ov8865->dev.fwnode->secondary = ERR_PTR(-ENODEV);
+	i2c_unregister_device(camera.ov8865);
 	surface_camera_unregister_nodes();
 }
 
