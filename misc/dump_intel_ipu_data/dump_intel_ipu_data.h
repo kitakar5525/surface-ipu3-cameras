224cc485a118698099cdd321050cf9a791549a48
Tsuchiya Yuto <kitakar@gmail.com>
2020-10-26 23:35:36 +0900

    dump_intel_ipu_data: add dump_dsm()
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- misc/dump_intel_ipu_data/dump_intel_ipu_data.h
+++ misc/dump_intel_ipu_data/dump_intel_ipu_data.h
@@ -148,3 +148,53 @@ const char *pld_shape_list[] = {
 };
 
 #define DSM_STR_BUF_SIZE 32
+
+/*
+ * _DSM that returns subsystem ID (found in sensor devices)
+ */
+#define SUBSYS_ID_DSM_REV			0x0
+#define SUBSYS_ID_DSM_RETURN_ID_FUNC		0x1
+/* SUBSYSTEM ID _DSM UUID: "822ace8f-2814-4174-a56b-5f029fe079ee" */
+static const guid_t subsys_id_dsm_guid =
+	GUID_INIT(0x822ace8f, 0x2814, 0x4174,
+		  0xa5, 0x6b, 0x5f, 0x02, 0x9f, 0xe0, 0x79, 0xee);
+
+/*
+ * _DSM that returns i2c dev data (found in sensors and non-discrete PMICs)
+ */
+#define I2C_DEV_DSM_REV			0x0
+/* The amount of devices defined in _CRS */
+#define I2C_DEV_DSM_DEV_AMOUNT_FUNC	0x1
+/* 0x2 and later is the funcs for getting i2c dev data. There are
+ * as many of funcs as the amount of i2c devices.
+ */
+/* I2C_DEV _DSM UUID: "26257549-9271-4ca4-bb43-c4899d5a4881" */
+static const guid_t i2c_dev_dsm_guid =
+	GUID_INIT(0x26257549, 0x9271, 0x4ca4,
+		  0xbb, 0x43, 0xc4, 0x89, 0x9d, 0x5a, 0x48, 0x81);
+
+/*
+ * _DSM that returns GPIO pin data (for discrete PMICs)
+ */
+#define DISCRETE_PMIC_DSM_REV			0x0
+/* The amount of devices defined in _CRS */
+#define DISCRETE_PMIC_DSM_GPIO_AMOUNT_FUNC	0x1
+/* 0x2 and later is the funcs for getting GPIO pin data. There are
+ * as many of funcs as the amount of GPIO pins.
+ */
+/* DISCRETE PMIC _DSM UUID: "79234640-9e10-4fea-a5c1-b5aa8b19756f" */
+static const guid_t pmic_dsm_guid =
+	GUID_INIT(0x79234640, 0x9e10, 0x4fea,
+		  0xa5, 0xc1, 0xb5, 0xaa, 0x8b, 0x19, 0x75, 0x6f);
+
+/*
+ * _DSM UUID found on some devices (Surface Go 1 / Go 2, Acer Switch
+ * Alpha 12 ...) (found in sensors and non-discrete PMICs)
+ */
+#define DSMB_DSM_REV			0x0
+/* This func returns a buffer called DSMB */
+#define DSMB_DSM_RETURN_BUF_FUNC	0x1
+/* DSMB _DSM UUID: "5815c5c8-c47d-477b-9a8d-76173176414b" */
+static const guid_t dsmb_dsm_guid =
+	GUID_INIT(0x5815c5c8, 0xc47d, 0x477b,
+		  0x9a, 0x8d, 0x76, 0x17, 0x31, 0x76, 0x41, 0x4b);
